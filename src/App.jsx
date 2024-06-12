2244e474e979b401deff1646333f822c284e2487
dt4real <dotchere95@gmail.com>
2024-06-12 16:33:12 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/porfolio-2024">
+    <BrowserRouter basename="/portfolio">
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
