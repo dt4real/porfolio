75cfbd5d26d738c0633236ccfda19e6e4e2b8c4a
dt4real <dotchere95@gmail.com>
2024-06-12 16:53:23 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1400, // Adjust the warning limit (in KB)
+  },
   base: '/portfolio/'
 })
